92022d51b0499d717849d2946fd370a92f44fdf0
TUSHAL NARESHBHAI SHARMA <tushalsharma12@gmail.com>
2025-04-19 20:00:55 +0530

    Update restrictAdminAccess.js

--- backend/middlewares/restrictAdminAccess.js
+++ backend/middlewares/restrictAdminAccess.js
@@ -1,1 +1,6 @@
-
+export const restrictAdminAccess = (req, res, next) => {
+    if (req.user.role === "admin") {
+      return res.status(403).json({ error: "Admins cannot access this resource" });
+    }
+    next();
+  };
