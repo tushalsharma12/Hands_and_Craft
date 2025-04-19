e2fa9fa6a02f4f848f9547f2fb7cb6d42687efbf
TUSHAL NARESHBHAI SHARMA <tushalsharma12@gmail.com>
2025-04-19 17:49:41 +0530

    Update Scroll_top.jsx

--- Frontend/src/components/utils/Scroll_top.jsx
+++ Frontend/src/components/utils/Scroll_top.jsx
@@ -7,7 +7,7 @@ function ScrollToTop() {
   useEffect(() => {
     window.scrollTo({
       top: 0,
-      behavior: "smooth", // Use "auto" if you want instant scroll
+      behavior: "smooth", 
     });
   }, [pathname]);
 
