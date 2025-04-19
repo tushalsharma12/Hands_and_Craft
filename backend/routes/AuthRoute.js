7bb0569e9f6704a4863b04592471bfed8cfa8047
TUSHAL NARESHBHAI SHARMA <tushalsharma12@gmail.com>
2025-04-19 20:04:06 +0530

    Update AuthRoute.js

--- backend/routes/AuthRoute.js
+++ backend/routes/AuthRoute.js
@@ -11,12 +11,18 @@ import {
 } from "../controllers/AuthController.js";
 import { authMiddleware } from "../middlewares/authMiddleware.js";
 import upload from "../middlewares/upload.js";
+export const restrictAdminAccess = (req, res, next) => {
+  if (req.user.role === "admin") {
+    return res.status(403).json({ error: "Admins cannot access this resource" });
+  }
+  next();
+};
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
-router.get("/profile", authMiddleware, getUserProfile); // Get user details
+router.get("/profile", authMiddleware,restrictAdminAccess, getUserProfile); // Get user details
 router.get("/users", getAllUsers);
 router.delete("/delete/:id", authMiddleware, deleteUser);
 router.post("/change-password", authMiddleware, updatePassword); // Update user password
