0a84cf077e8873b67da5608d9809035e1644d440
TUSHAL NARESHBHAI SHARMA <tushalsharma12@gmail.com>
2025-04-19 20:58:39 +0530

    Update CartRoute.js

--- backend/routes/CartRoute.js
+++ backend/routes/CartRoute.js
@@ -6,9 +6,18 @@ import {
 } from "../controllers/CartController.js";
 import { authMiddleware } from "../middlewares/authMiddleware.js";
 
+// authMiddleware.js
+export const restrictAdminAccess = (req, res, next) => {
+  if (req.user.role === "admin") {
+    return res.status(403).json({ error: "Admins cannot access this route" });
+  }
+  next();
+};
+
+
 const router = express.Router();
 
-router.get("/", authMiddleware, getCart);
+router.get("/", authMiddleware,restrictAdminAccess, getCart);
 router.post("/add", authMiddleware, addToCart);
 router.delete("/remove/:productId", authMiddleware, removeFromCart);
 
